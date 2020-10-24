0cf7f81185fd1f55dad3f6ee2c43ee7af6d72c26
assasinfil <nfa57643@gmail.com>
2020-10-24 18:33:29 +0300

    big cubic

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 )
 
 var (
-	seed = flag.Int("seed", 0, "seed for random generator. unix(now) be default")
+	seed  = flag.Int("seed", 0, "seed for random generator. unix(now) be default")
+	start = flag.Int("start", 1, "left default 1")
+	end   = flag.Int("end", 6, "right default 6")
 )
 
 // Фукнция должна вернуть число из интервала [l,r]
 func randInterval(l, r int) int {
-	return l + rand.Intn(r)
+	return l + rand.Intn(r+1)
 }
 
 func main() {
@@ -24,5 +26,9 @@ func main() {
 		rand.Seed(int64(*seed))
 	}
 	// Dice roll 1..6
-	fmt.Println(randInterval(1, 6))
+	if *start > *end {
+		fmt.Println("start > end")
+		return
+	}
+	fmt.Println(randInterval(*start, *end))
 }
